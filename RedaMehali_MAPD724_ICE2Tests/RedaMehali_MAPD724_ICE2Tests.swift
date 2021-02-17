cdb1864e131b8d11f5b2ccb2d6c8030a713aa8f2
Red7M <reda.mehali@gmail.com>
2021-02-16 19:22:03 -0500

    Initial Commit

---
+++ RedaMehali_MAPD724_ICE2Tests/RedaMehali_MAPD724_ICE2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  RedaMehali_MAPD724_ICE2Tests.swift
+//  RedaMehali_MAPD724_ICE2Tests
+//
+//  Created by Reda Mehali on 2/16/21.
+//
+
+import XCTest
+@testable import RedaMehali_MAPD724_ICE2
+
+class RedaMehali_MAPD724_ICE2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
